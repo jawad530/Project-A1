b70013e5900fc9c80d8e62ee6f846c2469fcb26d
Jawad Mazar <jawad530@gmail.com>
2019-02-09 00:37:08 -0800

    Setting up Basic Skeleton for Game() and GameWorld().

--- A1Prj/src/com/mycompany/a1/Game.java
+++ A1Prj/src/com/mycompany/a1/Game.java
@@ -4,4 +4,16 @@ import com.codename1.ui.Form;
 
 public class Game extends Form {
 
+	private GameWorld gw;
+	
+	public Game(){
+		gw = new GameWorld();
+		gw.init();
+		play();
+	}
+	
+	private void play() {
+		
+	}
+	
 }
--- A1Prj/src/com/mycompany/a1/GameWorld.java
+++ A1Prj/src/com/mycompany/a1/GameWorld.java
@@ -2,4 +2,8 @@ package com.mycompany.a1;
 
 public class GameWorld {
 
+	public void init() {
+		
+	}
+	
 }
