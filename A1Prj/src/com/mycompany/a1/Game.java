11254be694f6a1202a418562088a9f660d3d39b0
Jawad Mazar <jawad530@gmail.com>
2019-02-09 23:16:23 -0800

    Added Play() Method Skeleton code + GameObject() Variables and Functions

--- A1Prj/src/com/mycompany/a1/Game.java
+++ A1Prj/src/com/mycompany/a1/Game.java
@@ -1,6 +1,11 @@
 package com.mycompany.a1;
-
+import com.codename1.ui.events.ActionListener;
+import com.codename1.ui.Label;
+import com.codename1.ui.TextField;
+import com.codename1.ui.events.ActionEvent;
+import java.lang.String;
 import com.codename1.ui.Form;
+
 
 public class Game extends Form {
 
@@ -12,8 +17,29 @@ public class Game extends Form {
 		play();
 	}
 	
-	private void play() {
+	private void play() 
+	{
+		System.out.println("Welcome to the Game!");
+	    Label myLabel = new Label("Enter a Command:");
+	    this.addComponent(myLabel);
+	    final TextField myTextField = new TextField();
+	    this.addComponent(myTextField);
+	    this.show();
+	    myTextField.addActionListener(new ActionListener() {
+
+	            public void actionPerformed(ActionEvent evt) {
+
+	                String sCommand = myTextField.getText().toString();
+	                myTextField.clear();
+	                switch (sCommand.charAt(0)) {
+	                   // case 'e':
+	                     //   gw.eliminate();
+	                       // break;
+	                        //add code to handle rest of the commands 
+	                } //switch 
+	            } //actionPerformed 
+	        } //new ActionListener()  
+	    ); //addActionListener
+	} //play
 		
-	}
-	
 }
--- A1Prj/src/com/mycompany/a1/GameObject.java
+++ A1Prj/src/com/mycompany/a1/GameObject.java
@@ -1,5 +1,49 @@
 package com.mycompany.a1;
+import com.codename1.charts.util.ColorUtil;
 
 public abstract class GameObject {
 
+	// GameObject Variables
+	private float xLocation;
+	private float yLocation;
+	private int color;
+	
+	//Getter and Setter Methods:
+	
+	public void setXLoc(float x) 
+	{
+		
+	}
+	
+	public void setYLoc(float y) 
+	{
+		
+	}
+	
+	public void setObjectColor(int color) 
+	{
+		this.color = color;
+	}
+	
+	public float getXLoc(float x) 
+	{
+		return xLocation;
+	}
+	
+	public float getYLoc(float y) 
+	{
+		return yLocation;
+	}
+	
+	public int getObjectColor() 
+	{
+		return color;
+	}
+	
+	//Custom String Method
+	public String toString() 
+	{
+		//FIX THIS LATER
+		return null; //TO FIX*****************
+	}
 }
